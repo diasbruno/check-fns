5f97882cfef9143e42cfcf28596a26993aab7419
Bruno Dias <dias.h.bruno@gmail.com>
2021-11-20 21:02:04 -0300

    [refactor] use letters more consistent.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+// for the entire documentation...
+//
+// `f` means a function that will be used to check something.
+// `t` means every object `T(f)` which represents a test.
+// `i` an item to be tested e.g. `t(i) => result`.
+// `a` means the result of a test `t(i)`.
+// `m` means the success message.
+// `n` means the failure message.
+// `s` means the merge strategy. must implement `all` and `or`.
+// `k` can be an index or a key used to iterate over an object.
+// `r` means the result of a test
 const
 // Convert arguments to array.
 args = a => {
@@ -5,97 +16,90 @@ args = a => {
   for (let i = 0; i < len; i++) ls[i] = a[i];
   return ls;
 },
+// Return the message of X to an Array.
+resultMsgToArray = x => (
+  x && !x.result ? (x.msg.constructor != Array ? [x.msg] : x.msg) : []
+),
 // Given A and B, prefer the first result that is an error.
 keepFirst = {
-  all: (a, f, i, s, m) => a && !a.result ? a : f(i, s, m),
-  or: (a, f, i, s, m) => a && a.result ? a : f(i, s, m)
+  all: (a, t, i, m, s) => a && !a.result ? a : t(i, m, s),
+  or: (a, t, i, m, s) => a && a.result ? a : t(i, m, s)
 },
-// Return the message of X to an Array.
-resultMsgToArray = x =>
-  (x && !x.result ?
-   (x.msg.constructor != Array ? [x.msg] : x.msg) : []),
 // Given A and B, try to merge the results.
 collectAllErrors = {
-  all: (a, f, i, s, m) => {
-    const b = f(i, s, m);
+  all: (a, t, i, m, s) => {
+    const b = t(i, m, s);
     if (b.result) return a;
     return {
       msg: resultMsgToArray(a).concat(resultMsgToArray(b)),
       result: a.result && b.result
     };
   },
-  or: (a, f, i, s, m) => {
+  or: (a, t, i, m, s) => {
     if (a && a.result) return a;
-    const b = f(i, s, m);
-    if (!a || (b && b.result)) return b;
+    const b = t(i, m, s);
+    if (b && b.result) return b;
     return {
       msg: resultMsgToArray(a).concat(resultMsgToArray(b)),
       result: false
     };
   }
 },
-// Given `f : Function` and
-// `msg : any` (for the failed result)
-// to build a test function.
-// It can be called with just `B : any`,
-// or, with `_ : Function` (the merge function)
-// and `smsg : any` (success object).
-T = (f, msg) => (b, smsg, _) => {
-  const r = f(b);
-  return { msg: r ? smsg : msg, result: r };
+// Create a test object for a function.
+T = (f, n) => (i, m, _) => {
+  const r = f(i);
+  return { msg: r ? m : n, result: r };
 },
 // Given `o : Object`, it build a test function.
-object = o => (b, smsg, merge) =>
-  ((merge = merge || keepFirst), Object.keys(o).reduce(
-    (acc, c) => merge.all(acc, o[c], b[c], smsg, merge),
-    { msg: smsg, result: true }
+object = o => (i, m, s) =>
+  ((s = s || keepFirst), Object.keys(o).reduce(
+    (a, k) => s.all(a, o[k], i[k], m, s),
+    { msg: m, result: true }
   )),
 // Given `o : Array`, a pair of key name and test function,
 // it builds a test function.
-ordobject = o => (b, smsg, merge) => {
-  merge = merge || keepFirst;
-  let acc = { msg: smsg, result: true };
-  for (let i = 0; i < o.length; i += 2) {
-    acc = merge.all(acc, o[i + 1], b[o[i]], smsg, merge);
+ordobject = o => (i, m, s) => {
+  s = s || keepFirst;
+  let a = { msg: m, result: true };
+  for (let k = 0; k < o.length; k += 2) {
+    a = s.all(a, o[k + 1], i[o[k]], m, s);
   }
-  return acc;
+  return a;
 },
 // Given many test functions,
 // check if a given object pass all if them.
 all = function() {
-  return (b, smsg, merge) =>
-    ((merge = merge || keepFirst),
-     args(arguments).reduce(
-       (acc, c) => merge.all(acc, c, b, smsg, merge),
-       { msg: smsg, result: true }
-     ));
+  return (i, m, s) => (
+    (s = s || keepFirst),
+    args(arguments).reduce(
+      (a, t) => s.all(a, t, i, m, s),
+      { msg: m, result: true }
+    )
+  );
 },
 // Given many test functions,
 // check if a given object pass at least one of them.
 or = function() {
-  return (b, smsg, merge) =>
-    ((merge = merge || keepFirst),
-     args(arguments).reduce(
-       (acc, c) => merge.or(acc, c, b, smsg, merge),
-       null
-     ));
+  return (i, m, s) => (
+    (s = s || keepFirst),
+    args(arguments).reduce(
+      (a, t) => s.or(a, t, i, m, s),
+      null
+    )
+  );
 },
 // Given a test function and a message,
 // if the test fail, return the given message.
-ifFail = (f, msg) => (b, smsg, merge) => {
-  const r = f(b, smsg, merge);
-  return r.result ? r : { msg, result: false };
+ifFail = (f, n) => (i, m, s) => {
+  const r = f(i, m, s);
+  return r.result ? r : { msg: n, result: false };
 },
 // Validate combinator.
-// Given `f : Function`, `m: Function`
-// and `s : any` (for the failed result)
-// build a validation function that receives `b : any`
-// and return the result.
-V = (f, s, m) => b => ((m = m || keepFirst), m.or(null, f, b, s, m)),
+V = (t, m, s) => i => ((s = s || keepFirst), s.or(null, t, i, m, s)),
 // A validate function that return at the first error.
-validate = (f, b, s) => V(f, s, keepFirst)(b),
+validate = (t, i, m) => V(t, m, keepFirst)(i),
 // A validate function that collects all errors.
-validateAll = (f, b, s) => V(f, s, collectAllErrors)(b);
+validateAll = (t, i, m) => V(t, m, collectAllErrors)(i);
 
 module.exports = {
   T,
